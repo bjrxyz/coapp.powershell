cb493943c230c58f7d2f0af57f7b958f6caa83ee
bjr <bjrxyz@sina.com>
2016-07-02 00:49:12 +0800

    [modify]添加注释

--- tools/replace.cmd
+++ tools/replace.cmd
@@ -1,4 +1,5 @@
 :: Created by npm, please don't edit manually.
+:: 生成后事件调用，用来将代码文件中的版本后自动更新
 @IF EXIST "%~dp0\node.exe" (
   "%~dp0\node.exe"  "%~dp0\node_modules\replace\bin\replace.js" %*
 ) ELSE (
